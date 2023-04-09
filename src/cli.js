3328af0b06143192ccfd5a9d402e49093e94b710
FrancoTastaca <franc0tastac@gmail.com>
2023-04-09 19:18:18 -0300

    fix: Se soluciono el bug que permitia seleccionar una funcion no disponible
    Dentro del codigo explico un poco mas a detalle lo que hice

--- src/cli.js
+++ src/cli.js
@@ -15,7 +15,13 @@ async function loop() {
         console.log("👋👋👋");
         return readline.close();
     }
-    
+    if (!fn) {
+        console.log(`La función "${fnName}" es incorrecta, por favor vuelva intentar`);
+        return loop();
+    }
+    /*La función const fn = core[fnName]; toma el valor de core[fnName], es decir, toma el valor de la propiedad con nombre igual al valor de la variable fnName dentro del objeto core. Si fnName es 'add', fn tomará el valor de la función add dentro de core.
+    Luego, el if (!fn) verifica si fn es falso, lo que sucederá si core[fnName] no existe o es undefined. Si es así, se imprimirá un mensaje de error en la consola con el nombre de la función incorrecta, y se llamará a la función loop() para volver a pedir al usuario que ingrese una función válida. */ 
+
     else if (fnName == "pow"){
             const powNum = await readline.question("Ingrese el número a elevar al cuadrado: ");
              //Se trata el bug de solicitar mas parametros de los necesarios para el pow cuadrado
