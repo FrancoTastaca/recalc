cf01089d46e23cdba8569be373eb73a60f1cdf9b
FrancoTastaca <franc0tastac@gmail.com>
2023-05-24 19:23:52 -0300

    test: Se realizo el test para comprobar lo pedido y paso con exito

--- test/api.test.js
+++ test/api.test.js
@@ -20,3 +20,19 @@ describe("API substract", () => {
             })
     })
 })
+
+describe("API add", () => {
+    test("El segundo parámetro es negativo, por lo que el resultado debe ser menor al primer parametro por ser una suma y el endpoint debe devolver un status 200", async () => {
+        const app = await api.build();
+
+        request(app).get('/api/v1/add/10/-5')
+            .expect(200)
+            .expect('Content-Type', "application/json; charset=utf-8")
+            .end((err, res) => {
+                if (err) throw err;
+
+                expect(res.body.result).toBeLessThan(10); 
+            })
+    });
+});
+
