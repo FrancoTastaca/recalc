43e11fed4ee550862e6c0912bfecee0dd6f9badd
FrancoTastaca <franc0tastac@gmail.com>
2023-05-24 19:13:06 -0300

    fix: Se modifica el endopoint de add y models para guardar la entrada al historial

--- src/controllers.js
+++ src/controllers.js
@@ -22,7 +22,6 @@ router.get("/sub/:a/:b", validacionParametrosNum, async function (req, res) {
     const { a, b } = req.validParams;
     try {
         const result = core.sub(a, b);
-        await createHistoryEntry({ firstArg: a, operationName: "ADD" })
         res.send({ result });
     } catch (error) {
         res.status(400).send({ error: 'Uno de los parámetros de la resta no es un número. Por favor, asegurese de que ambos parámetros sean válidos' });
@@ -33,6 +32,7 @@ router.get("/add/:a/:b", validacionParametrosNum, async function (req, res) {
     const { a, b } = req.validParams;
     try {
         const result = core.add(a, b);
+        await createHistoryEntry({ firstArg: a, secondArg: b, operationName: "ADD", result})
         res.send({ result });
     } catch (error) {
         res.status(400).send({ error: 'Uno de los parámetros de la suma no es un número. Por favor, asegurese de que ambos parámetros sean válidos' });
