468130fe076e79336cf5222a0efb39d5a026e7f7
FrancoTastaca <franc0tastac@gmail.com>
2023-05-26 20:43:43 -0300

    refactor: Actualizo codigo pisado por los marge

--- src/controllers.js
+++ src/controllers.js
@@ -23,12 +23,9 @@ function validacionParametrosNum(req, res, next) {
 
 router.get("/sub/:a/:b", validacionParametrosNum, async function (req, res) {
     const { a, b } = req.validParams;
-    try {
-        const result = core.sub(a, b);
-        res.send({ result });
-    } catch (error) {
-        res.status(400).send({ error: 'Uno de los parámetros de la resta no es un número. Por favor, asegurese de que ambos parámetros sean válidos' });
-    }
+    const result = core.sub(a, b);
+    res.send({ result });
+    
 });
 
 router.get("/add/:a/:b", validacionParametrosNum, async function (req, res) {
