255d5b1d0c69702fdbe65b346b8a402c9a2b7364
FrancoTastaca <franc0tastac@gmail.com>
2023-05-26 21:35:27 -0300

    fix: Modifico los test cambiando end por then y elimino el if(err)

--- test/api.test.js
+++ test/api.test.js
@@ -10,12 +10,10 @@ describe("API substract", () => {
     test("Deberia responder con un 200 ok", async () => {
         const app = await api.build()
 
-        request(app).get('/api/v1/sub/2/1')
+        return request(app).get('/api/v1/sub/2/1')
             .expect(200)
             .expect('Content-Type', "application/json; charset=utf-8")
-            .end((err, res) => {
-                if (err) throw err
-
+            .then((res) => {
                 expect(res.body.result).toEqual(1);
             })
     })
@@ -25,26 +23,22 @@ describe("API add", () => {
     test("El segundo parámetro es negativo, por lo que el resultado debe ser menor al primer parametro por ser una suma y el endpoint debe devolver un status 200", async () => {
         const app = await api.build();
 
-        request(app).get('/api/v1/add/10/-5')
+        return request(app).get('/api/v1/add/10/-5')
             .expect(200)
             .expect('Content-Type', "application/json; charset=utf-8")
-            .end((err, res) => {
-                if (err) throw err;
+            .then((res) => {
                 expect(res.body.result).toBeLessThan(10); 
             })
     });
 });
 
 describe("API Mul", () => {
-    test("Ambos parametros son decimales, por lo que el resultado debe ser con decimales y el endpoint debe devolver un status 200", async () => {
+    test("Ambos parametros son decimales, por lo que el resultado debe ser con decimales y el thenpoint debe devolver un status 200", async () => {
         const app = await api.build();
-
-        request(app).get('/api/v1/mul/2.124/4.757')
+        return request(app).get('/api/v1/mul/2.124/4.757')
             .expect(200)
             .expect('Content-Type', "application/json; charset=utf-8")
-            .end((err, res) => {
-                if (err) throw err;
-          
+            .then((res) => {
                 expect(Number.isInteger(res.body.result)).toBe(false) 
             })
     });
